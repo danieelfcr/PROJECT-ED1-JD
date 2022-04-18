295b0d353bba6740ddbdd7a57b03aa525d898b8a
Jose Mario Marroquin <jmmr114@gmail.com>
2022-04-17 23:05:04 -0600

    Patient model created

---
+++ ClassLibrary/Patient.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ClassLibrary
+{
+    
+    // ================== Patient Model ===================
+              
+    public class Patient
+    {
+        public string FullName { get; set; } //required
+        public int DPI { get; set; } //required
+        public int Age { get; set; } //required
+        public int PhoneNumber { get; set; } //required
+        public DateTime LastConsultation { get; set; } //required
+        public DateTime NextConsultation { get; set; } //optional
+        public string TreatmentDescription { get; set; } //optional
+    }
+}
