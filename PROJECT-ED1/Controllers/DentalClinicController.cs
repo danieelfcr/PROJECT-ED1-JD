cb23a8e0de2145ef1f3981ccc706b1919919e5ce
danieelfcr <danielfcab2002@gmail.com>
2022-04-27 15:23:25 -0600

    AVL Insertion implemented

--- PROJECT-ED1/Controllers/DentalClinicController.cs
+++ PROJECT-ED1/Controllers/DentalClinicController.cs
@@ -4,15 +4,17 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using ClassLibrary;
+using PROJECT_ED1.Helpers;
 
 namespace PROJECT_ED1.Controllers
 {
     public class DentalClinicController : Controller
     {
         // GET: DentalClinicController
-        public ActionResult Index()
+        public ActionResult Index(List<Patient> PatientList)
         {
-            return View();
+            return View(Data.Instance.DPITree.NodeList); //Just to verify implementation
         }
 
         // GET: DentalClinicController/Details/5
@@ -24,7 +26,7 @@ namespace PROJECT_ED1.Controllers
         // GET: DentalClinicController/Create
         public ActionResult Create()
         {
-            return View();
+            return View(new Patient());
         }
 
         // POST: DentalClinicController/Create
@@ -34,6 +36,31 @@ namespace PROJECT_ED1.Controllers
         {
             try
             {
+                Patient patient = new Patient
+                {
+                    FullName = collection["FullName"],
+                    DPI = Convert.ToInt64(collection["DPI"]),
+                    Age = Convert.ToInt32(collection["Age"]),
+                    PhoneNumber = Convert.ToInt64(collection["PhoneNumber"]),
+                    LastConsultation = Convert.ToDateTime(collection["LastConsultation"]),
+                    NextConsultation = Convert.ToDateTime(collection["NextConsultation"]),
+                    TreatmentDescription = collection["TreatmentDescription"]
+
+                };
+
+                Node<Patient> NewNodeDPI = new Node<Patient>(patient);
+                Node<Patient> NewNodeName = new Node<Patient>(patient);
+
+                Data.Instance.DPITree.Root = Data.Instance.DPITree.Insert(Data.Instance.DPITree.Root, NewNodeDPI);   //Call to insert to DPI TREE function
+                Data.Instance.NameTree.Root = Data.Instance.NameTree.Insert(Data.Instance.DPITree.Root, NewNodeName);   //Call to insert to NAME TREE function
+
+                //Clear list
+                Data.Instance.DPITree.NodeList.Clear();
+
+                //Fill list
+                Data.Instance.DPITree.InOrder(Data.Instance.DPITree.Root);
+
+
                 return RedirectToAction(nameof(Index));
             }
             catch
