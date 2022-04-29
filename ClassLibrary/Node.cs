e3763524d934de2e7b9246c1af10cb383f671ac4
Jose Mario Marroquin <jmmr114@gmail.com>
2022-04-29 03:13:59 -0600

    bugs fixed in insertion method

--- ClassLibrary/Node.cs
+++ ClassLibrary/Node.cs
@@ -10,14 +10,9 @@ namespace ClassLibrary
         public Node<T> Left;
         public Node<T> Right;
 
-        public int BalanceFactor { get; set; }
-        public int Height { get; set; }
-
         public Node(T Record)
         {
             this.Record = Record;
-            BalanceFactor = 0;
-            Height = 1;
         }
     }
 }
